bd5ae91dec55d50e6531ff1fecfdce0d51397498
shreedhar <krishna.shreedhar@gmail.com>
2020-08-08 12:56:53 +0530

    Add ticket_generator.py

---
+++ src/ticket_system/ticket_generator.py
@@ -0,0 +1,32 @@
+import json
+
+
+class TicketGenerator:
+
+    def __init__(self, source, destination, start_time, **kwargs):
+        self.source = source
+        self.destination = destination
+        self.start_time = start_time
+        self.end_time = kwargs.get('end_time', None)
+        self.max_hops = kwargs.get('max_hops', 9999)
+        self.max_cost = kwargs.get('max_cost', 9999)
+        self.is_senior_citizen = kwargs.get('is_senior_citizen', False)
+        self.is_adult = kwargs.get('is_adult', True)
+
+        self.dict_ticket = {}
+
+    def get_dict_ticket(self):
+        if not self.dict_ticket:
+            self.dict_ticket = self.compute_dict_ticket()
+        return self.dict_ticket
+
+    def compute_dict_ticket(self):
+        dict_ticket = {
+            'source': self.source,
+            'destination': self.destination,
+            'cost': 100.0
+        }
+        return dict_ticket
+
+    def get_json_ticket(self):
+        return json.dumps(self.get_dict_ticket())
